b9a0af0ae3184ebeccce9ace1fd2a167de81353d
Prashay <113265973+wadesword89@users.noreply.github.com>
2023-09-24 12:59:23 -0700

    complete RPS functionality

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Rock-Paper-Scissors
+
+implementation of “rock paper scissors”. I
+play a 5 round game against a computer.  keeps score and reports a winner or loser at the end.
--- main.js
+++ main.js
@@ -2,27 +2,47 @@
 function game() {
   
     const options = ["rock", "paper", "scissors"];
+    let round = 0;
+    let userScore = 0;
+    let compScore = 0;
+
+    while(round < 5) {
+        const computerSelection = getComputerChoice();
+        const input = prompt("Please Enter Choice:");
+        console.log(playRound(input, computerSelection));
+        if(playRound(input, computerSelection) !== "Invalid Selection. Please retry") {
+            round++;
+        }
+    }
+
     function getComputerChoice() {
       return options[Math.floor(Math.random() * options.length)];
     }
-    function playRound(playerSelection) {
+
+    function playRound(playerSelection, computerSelection) {
         const selection = playerSelection.toLowerCase();
         if(!options.includes(selection)) return "Invalid Selection. Please retry";
-        if (selection === computerSelection) return `You both chose ${selection}! It's a draw!`;
+        if (selection === computerSelection) return `Draw! You both chose ${selection}!`;
         if (selection === "rock" && computerSelection === "paper") {
+            compScore++;
             return `You Lose! ${computerSelection} beats ${selection}`;
         }
         else if (selection === "paper" && computerSelection === "scissors") {
+            compScore++;
             return`You Lose! ${computerSelection} beats ${selection}`;
         }
         else if (selection === "scissors" && computerSelection === "rock") {
+            compScore++;
             return `You Lose! ${computerSelection} beats ${selection}`;
         }
-        else return `You Win! ${selection} beats ${computerSelection}`;
+        else {
+            userScore++;
+            return `You Win! ${selection} beats ${computerSelection}`;
+        }
     }
-    const input = prompt("Please Enter Choice:");
-    const computerSelection = getComputerChoice();
-    return console.log(playRound(input));
+
+    const result = userScore > compScore ? "You win!" : userScore < compScore ? "You lose!" : "It's a draw!";
+    return `Final score: You: ${userScore}, Computer: ${compScore}\n${result}`;
   }
   
-  game();
+  console.log(game());
